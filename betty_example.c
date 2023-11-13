af63b1294bc0a7bd4b753b218849dd8e346ee599
Eric <ericgeorge4477@gmail.com>
2023-11-13 20:32:34 +0200

    betty_example.c

---
+++ betty_example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char *message = "Hello, Betty!";
+
+    printf("%s\n", message);
+
+    return (0);
+}
